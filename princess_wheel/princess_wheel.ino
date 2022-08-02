9283725bfda5220adada9d686b51e15749ed5e05
chloebee123 <itschloebelletti@gmail.com>
2022-08-02 19:10:32 -0400

    NOT YET FUNCTIONAL. Still need to implement LCD Update function as well as function calls in the loop

--- princess_wheel/princess_wheel.ino
+++ princess_wheel/princess_wheel.ino
@@ -1,24 +1,49 @@
 #include <LiquidCrystal.h>
 #include <TimeLib.h>
+#include <EEPROM.h>
 
-volatile int counter = 0, miles = 0, dailyHighscore = 0;
-const double pi = 3.1415926535;
-const double rev = 9 * pi; // inches of distance per revolution
+// initializing counter 
+volatile int counter = 0;
+
+// initializing scores
+float dailyMiles = 0;
+float Highscore = 0;
+float totalMiles = 0;
+
+
+const float pi = 3.1415926535;
+const float rev = 9 * pi; // inches of distance per revolution
+
+// initializing pins
 const int HEpin = 2, LCDrx = 0, LCDtx = 1;
 
 
-
 void setup() {
+  // getting dailyMiles data stored in R0
+  dailyMiles = EEPROM.read(0);
+  // getting totalMiles data stored in R1
+  totalMiles = EEPROM.read(1);
+  // getting highscore data stored in R2
+  Highscore = EEPROM.read(2);
+  
   Serial.begin(9600);
-  pinMode(HEpin, INPUT); // Hall effect sensor as input
+  
+  // Hall effect sensor as input
+  pinMode(HEpin, INPUT); 
+  
   setTime(5, 27, 30, 2, 8, 2022); // setting time
-  attachInterrupt(digitalPinToInterrupt(HEpin), boop, RISING); // Interrupt which increments counter on sensor input
+  
+  // Interrupt which increments counter on sensor input
+  attachInterrupt(digitalPinToInterrupt(HEpin), boop, RISING); 
 }
 
 void loop() {
-  Serial.print("Total Revolutions: ");
+  Serial.print("Daily Miles: ");
+  Serial.println(dailyMiles);
+  Serial.print("Counter: ");
   Serial.println(counter);
-  delay(100);
+  delay(1000);
+  UpdateCount();
 }
 
 void boop() {
@@ -27,13 +52,34 @@ void boop() {
 }
 
 void DailyReset () { // reset counter and store highscores/other data
+  // saving Highscore val:
+  if (dailyMiles > Highscore) {
+    Highscore = dailyMiles;
+  }
+  EEPROM.write(2, Highscore);
+
+  // saving total Miles
+  EEPROM.write(1, totalMiles);
+
+  // resetting others
+  counter = 0;
+  dailyMiles = 0;
   
 }
 
-void UpdateLCD() { // updates lcd every five seconds
-  
+void UpdateLCD() { // updates lcd 
+  // slade
 }
 
-void Revs2Miles(counter, rev){
+void UpdateCount() { // updates daily / total vals to system and resets tmp and counter
+  dailyMiles = dailyMiles + (counter * rev  );//63360);
+  totalMiles = totalMiles + (counter * rev / 63360);
+  counter = 0;
+  
+  // saving dailyMiles val
+  EEPROM.write(0, dailyMiles);
+
+  // update totalMiles
+  EEPROM.write(1, totalMiles);
   
 }
